13b7c2bcd7a74842c2de6bd1edecafe50bd941ab
Zhulinjiuying <554851075@qq.com>
2018-01-09 23:12:25 +0800

    修改代码结构

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,22 @@
 
-var router = function(event, page) {
+var router = function(element, page) {
     //路由函数
-    if (event.target.className.indexOf('active') == -1) {    
+    if (element.className.indexOf('active') == -1) {    
         switchPage(page)
         // pushState(page, 'switch')
         // toggleMenu()
     }
 }
 
-var play = function(event) {
+var play = function(element) {
     var img = e('.footer .img .music-img')
-    if (event.target.className.indexOf('pause') == -1) {
+    if (element.className.indexOf('pause') == -1) {
         player.player.play()
     } else {
         player.player.pause()
     }
     toggleClass(img, 'play')
-    toggleClass(event.target, 'pause')
+    toggleClass(element, 'pause')
 }
 
 var changeTime = function(event) {
@@ -24,7 +24,6 @@ var changeTime = function(event) {
     let coordStart = progressBar.getBoundingClientRect().left
     let coordEnd = event.pageX
     player.player.currentTime = (coordEnd - coordStart) / progressBar.offsetWidth * player.player.duration
-    // e('.now').innerText = player.transformTime(player.player.currentTime)
     player.player.play();
 }
   
@@ -37,7 +36,6 @@ var touchMove = function(event) {
 
 var touchEnd = function(event) {
     player.player.currentTime = e('.now').style.width.replace('%', '')/100 * player.player.duration
-    // e('.now').innerText = player.transformTime(player.player.currentTime)
     player.player.play()
 }
 
@@ -119,24 +117,6 @@ var pushState = function(page, target) {
 }
 
 var initApp = function() {
-    // 根据地址栏的参数来显示不同的页面
-    var query = location.search
-    var [k, v] = query.slice(1).split('=')
-    var validPages = ['news', 'details']
-    if (k == 'page') {
-        for (let i = 0; i < validPages.length; i++) {
-            if (validPages[i] == v) {
-                switchPage(validPages[i])
-                pushState(validPages[i], 'init')
-            }
-        }
-    } else {
-        switchPage('index')
-        pushState('index', 'init')
-    }
-}
-
-var _main = function() {
     var musicData = [
         {
             'musicSrc': './static/music/時を刻む唄.mp3',
@@ -150,7 +130,7 @@ var _main = function() {
         }
     ]
     localStorage.musicData = JSON.stringify(musicData)
-    window.player = new Player()
+    player = new Player()
     switchMusic(0)
     player.player.addEventListener("canplay", () => {
         e('.end').innerText = player.transformTime(player.player.duration)
@@ -165,8 +145,25 @@ var _main = function() {
     player.player.addEventListener('ended', () => {
         toggleClass(e('.play-icon'), 'pause')
     })
-    
-    // initApp()
+    // 根据地址栏的参数来显示不同的页面
+    var query = location.search
+    var [k, v] = query.slice(1).split('=')
+    var validPages = pageList
+    if (k == 'page') {
+        for (let i = 0; i < validPages.length; i++) {
+            if (validPages[i] == v) {
+                switchPage(validPages[i])
+                // pushState(validPages[i], 'init')
+            }
+        }
+    } else {
+        switchPage(validPages[0])
+        // pushState('index', 'init')
+    }
+}
+
+var _main = function() {
+    initApp()
     // window.addEventListener("popstate", function(e) {
     //     var state = e.state;
     //     // state 就是 pushState 的第一个参数
